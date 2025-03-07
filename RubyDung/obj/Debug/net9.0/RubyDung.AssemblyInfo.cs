ff8b5ae5cb8f7c80f981a5f810e7ae40798d56e7
Morgana Stradivarius <55167884+Morgs6000@users.noreply.github.com>
2025-03-07 09:43:20 -0300

    Gerando um Mundo
    Agora geramos um mundo com 256 x 64 x 256 blocos (ou 16 x 4 x 16 chunks). a pedra é gerada em 2/3 do mundo até a camada y41, grama é gerada em y42 e o restante é ar

--- RubyDung/obj/Debug/net9.0/RubyDung.AssemblyInfo.cs
+++ RubyDung/obj/Debug/net9.0/RubyDung.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RubyDung")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4684da984f62b4a15a1d604cccb7f6b51f181e8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5205d19b85cb0b56a15c3ab1647508a65bff04bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("RubyDung")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RubyDung")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
