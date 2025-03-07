ca2aad842583e7a04a7d75cdc9ac650dbed7e878
Morgana Stradivarius <55167884+Morgs6000@users.noreply.github.com>
2025-03-07 09:24:56 -0300

    Icone de Janela
    Adicionado um Icone de Janela com a logo da biblioteca OpenTK

--- RubyDung/obj/Debug/net9.0/RubyDung.AssemblyInfo.cs
+++ RubyDung/obj/Debug/net9.0/RubyDung.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RubyDung")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b00da71fb9f951f00338afb8d5f34ec3f07a369")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4527cbfe03c90ee3cb33d31bc0ba41cd6f56bdd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("RubyDung")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RubyDung")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
