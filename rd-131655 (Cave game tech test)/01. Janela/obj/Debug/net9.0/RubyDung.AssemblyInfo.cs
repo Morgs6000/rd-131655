3686b69b208228c311f48f257661bca0d11a9714
Morgana Stradivarius <55167884+Morgs6000@users.noreply.github.com>
2025-03-25 07:22:58 -0300

    Commit Update

--- rd-131655 (Cave game tech test)/01. Janela/obj/Debug/net9.0/RubyDung.AssemblyInfo.cs
+++ rd-131655 (Cave game tech test)/01. Janela/obj/Debug/net9.0/RubyDung.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RubyDung")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c81454e3122a83d0649b82b419d6d72a049da92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+342c29f277df9dddae155d51c22ad14475292be1")]
 [assembly: System.Reflection.AssemblyProductAttribute("RubyDung")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RubyDung")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- rd-131655 (Cave game tech test)/18. AABB (colisão aparentemente certa, movimento cagado)/obj/Debug/net9.0/RubyDung.AssemblyInfo.cs
+++ rd-131655 (Cave game tech test)/18. AABB (colisão aparentemente certa, movimento cagado)/obj/Debug/net9.0/RubyDung.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RubyDung")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fab5d30e864aa3ac6d151d602b14afbeef2edbe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+342c29f277df9dddae155d51c22ad14475292be1")]
 [assembly: System.Reflection.AssemblyProductAttribute("RubyDung")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RubyDung")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
